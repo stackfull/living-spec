1dae8bb4eb28b57af05086055212e852ad8c9866
paul <paul@stackfull.com>
2016-02-14 10:22:50 +0000

    support deep linking

--- templates/static/toc.js
+++ templates/static/toc.js
@@ -1,11 +1,22 @@
 'use strict';
-$(document).foundation();
+$(document).ready(function($) {
+  
+  $(document).foundation();
 
-var featureLinks = $('.feature-link');
-var featureContent = $('.feature-content');
-featureLinks.click(function() {
-  featureContent.hide();
-  $($(this).attr('href')).show();
+  var featureLinks = $('.feature-link');
+  var featureContent = $('.feature-content');
+  featureLinks.click(function() {
+    featureContent.hide();
+    $($(this).attr('href')).show();
+  });
+  var hash = window.location.hash;
+  if (hash != '') {
+    var item = $('[data-accordion-menu] [href="' + hash + '"]');
+    item.click()
+        .parents('.is-accordion-submenu-parent')
+        .children('a')
+        .click()
+  }
 });
 
 
